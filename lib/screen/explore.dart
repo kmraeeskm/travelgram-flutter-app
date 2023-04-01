bc82bb9777d2a8d358780f1d6ba9fa6c70c92b94
kmraeeskm <kmraeeskm@gmail.com>
2023-04-01 15:42:52 +0530

    savepoint

--- lib/screen/explore.dart
+++ lib/screen/explore.dart
@@ -2,11 +2,28 @@
 
 import 'package:boxicons/boxicons.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:travelgram/screen/home_screen.dart';
+
+class Post {
+  final String name;
+  final String location;
+  final String imageurl;
+  final String bio;
+  final String dpurl;
+
+  Post({
+    required this.name,
+    required this.location,
+    required this.imageurl,
+    required this.bio,
+    required this.dpurl,
+  });
+}
 
 class Explore extends StatefulWidget {
   const Explore({super.key});
@@ -16,6 +33,44 @@ class Explore extends StatefulWidget {
 }
 
 class _ExploreState extends State<Explore> {
+  final List<Post> _posts = [];
+  String searchText = "";
+
+  Future<void> _fetchPosts() async {
+    try {
+      final postDocs =
+          await FirebaseFirestore.instance.collection('posts').get();
+
+      for (var postDoc in postDocs.docs) {
+        final userId = postDoc['uId'];
+
+        final userDoc = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userId)
+            .get();
+        final userData = userDoc.data() as Map<String, dynamic>;
+        final post = Post(
+          dpurl: userData['photourl'],
+          bio: postDoc['description'],
+          imageurl: postDoc['imageUrl'],
+          location: postDoc['location'],
+          name: userData['username'],
+        );
+        _posts.add(post);
+      }
+
+      setState(() {});
+    } catch (e) {
+      print('Error fetching posts: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,32 +100,111 @@ class _ExploreState extends State<Explore> {
           ),
         ],
       ),
-      body: FutureBuilder(
-        future: FirebaseFirestore.instance.collection('posts').get(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return MasonryGridView.builder(
-                itemCount: snapshot.data!.size,
-                gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                ),
-                itemBuilder: (context, index) {
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+              child: CupertinoSearchTextField(
+                onChanged: (value) {
+                  setState(() {
+                    searchText = value;
+                  });
+                },
+                borderRadius: BorderRadius.circular(10.0),
+                placeholder: 'Search places',
+              ),
+            ),
+          ),
+          Expanded(
+            child: MasonryGridView.builder(
+              itemCount: _posts.length,
+              gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemBuilder: (context, index) {
+                if (searchText == '' ||
+                    _posts[index]
+                        .location
+                        .toLowerCase()
+                        .contains(searchText.toLowerCase())) {
                   return Padding(
                     padding: const EdgeInsets.all(2.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.network(
-                        (snapshot.data! as dynamic).docs[index]['imageUrl'],
-                      ),
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            _posts[index].imageurl,
+                          ),
+                        ),
+                        Positioned(
+                          top: 5,
+                          left: 5,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 10,
+                                    backgroundImage:
+                                        NetworkImage(_posts[index].dpurl),
+                                  ),
+                                  Text(
+                                    _posts[index].location,
+                                    style: TextStyle(fontSize: 10),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   );
-                });
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+                }
+                return Container();
+              },
+            ),
+          ),
+          // Expanded(
+          //   child: FutureBuilder(
+          //     future: FirebaseFirestore.instance.collection('posts').get(),
+          //     builder: (context, snapshot) {
+          //       if (snapshot.hasData) {
+          //         return MasonryGridView.builder(
+          //             itemCount: snapshot.data!.size,
+          //             gridDelegate:
+          //                 SliverSimpleGridDelegateWithFixedCrossAxisCount(
+          //               crossAxisCount: 3,
+          //             ),
+          //             itemBuilder: (context, index) {
+          //               return Padding(
+          //                 padding: const EdgeInsets.all(2.0),
+          //                 child: ClipRRect(
+          //                   borderRadius: BorderRadius.circular(10),
+          //                   child: Image.network(
+          //                     (snapshot.data! as dynamic).docs[index]
+          //                         ['imageUrl'],
+          //                   ),
+          //                 ),
+          //               );
+          //             });
+          //       } else {
+          //         return Center(
+          //           child: CircularProgressIndicator(),
+          //         );
+          //       }
+          //     },
+          //   ),
+          // ),
+        ],
       ),
     );
   }
