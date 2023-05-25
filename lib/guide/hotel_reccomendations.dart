56dcd5105bcc8b55bb1c03d00cf43b2361a0ba7b
kmraeeskm <kmraeeskm@gmail.com>
2023-05-25 13:28:50 +0530

    hotels reccomendation bug fix

--- lib/guide/hotel_reccomendations.dart
+++ lib/guide/hotel_reccomendations.dart
@@ -3,28 +3,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:travelgram/screen/hotel/hotel_details.dart';
 
 class Hotel {
-  final List<String> bookings;
+  // final List<String> bookings;
   // final List<String> subPics;
   final String description;
   final String imageUrl;
   final String location;
-  final List<dynamic> likes;
-  final String name;
+  // final List<dynamic> likes;
+  // final String name;
   final String postId;
   final int roomCount;
   final Timestamp time;
   final String uId;
 
   Hotel({
-    required this.bookings,
+    // required this.bookings,
     // required this.subPics,
     required this.description,
     required this.imageUrl,
     required this.location,
-    required this.likes,
-    required this.name,
+    // required this.likes,
+    // required this.name,
     required this.postId,
     required this.roomCount,
     required this.time,
@@ -42,6 +43,7 @@ class HotelReccomendations extends StatefulWidget {
 
 class _HotelReccomendationsState extends State<HotelReccomendations> {
   final List<Hotel> _foods = [];
+  final List<QueryDocumentSnapshot<Map<String, dynamic>>> _foods2 = [];
   String searchText = "";
 
   Future<void> _fetchPosts() async {
@@ -53,30 +55,30 @@ class _HotelReccomendationsState extends State<HotelReccomendations> {
           .collection('hotels')
           .where('location', isEqualTo: loc)
           .get();
-      print(loc);
-      print(postDocs.docs);
       for (var postDoc in postDocs.docs) {
         final userId = postDoc['uId'];
-
-        final userDoc =
-            await FirebaseFirestore.instance.collection('users').get();
-        final userData = postDoc.data() as Map<String, dynamic>;
-        List<String> bookins = [];
+        print(userId);
+        final userDoc = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userId)
+            .get();
+        final userData = userDoc.data() as Map<String, dynamic>;
+        // List<String> bookins = [];
         // final rating = postDoc['rating'].toString();
         // final ratingC = postDoc['ratingCount'].toString();
         // final price = postDoc['price'].toString();
-        for (var x in userData['bookings']) {
-          bookins.add(x);
-        }
-        print(bookins);
+        // for (var x in userData['bookings']) {
+        //   bookins.add(x);
+        // }
+        // print(bookins);
         final post = Hotel(
           // subPics: postDoc['subPics'],
           description: postDoc['description'],
-          bookings: bookins,
+          // bookings: bookins,
           imageUrl: postDoc['imageUrl'],
           location: postDoc['location'],
-          likes: postDoc['likes'],
-          name: postDoc['name'],
+          // likes: postDoc['likes'],
+          // name: userData['name'],
           postId: postDoc['postId'],
           uId: postDoc['uId'],
           time: postDoc['time'],
@@ -85,6 +87,7 @@ class _HotelReccomendationsState extends State<HotelReccomendations> {
         //thiruvananthapuram,kerala
         //thriuvananthapuram,kerala
         _foods.add(post);
+        _foods2.add(postDoc);
       }
 
       setState(() {});
@@ -121,41 +124,52 @@ class _HotelReccomendationsState extends State<HotelReccomendations> {
                           .contains(searchText.toLowerCase())) {
                     return Padding(
                       padding: const EdgeInsets.all(2.0),
-                      child: Stack(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(
-                              _foods[index].imageUrl,
-                            ),
-                          ),
-                          Positioned(
-                            top: 5,
-                            left: 5,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                borderRadius: BorderRadius.circular(10),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (_) => HotelDetails(
+                                snap: _foods2[index],
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(4.0),
-                                child: Row(
-                                  children: [
-                                    CircleAvatar(
-                                        radius: 10,
-                                        backgroundColor: Colors.green[100]
-                                        // NetworkImage(_foods[index].dpurl),
-                                        ),
-                                    Text(
-                                      _foods[index].location,
-                                      style: TextStyle(fontSize: 10),
-                                    )
-                                  ],
+                            ),
+                          );
+                        },
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                _foods[index].imageUrl,
+                              ),
+                            ),
+                            Positioned(
+                              top: 5,
+                              left: 5,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Row(
+                                    children: [
+                                      CircleAvatar(
+                                          radius: 10,
+                                          backgroundColor: Colors.green[100]
+                                          // NetworkImage(_foods[index].dpurl),
+                                          ),
+                                      Text(
+                                        _foods[index].location,
+                                        style: TextStyle(fontSize: 10),
+                                      )
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     );
                   }
